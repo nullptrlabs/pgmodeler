1a2144e480b2b4ac0208dfab8ac4580d9f5857c8
Raphael Araújo e Silva <raphael@pgmodeler.io>
2025-11-04 09:31:31 -0300

    Minor adjustment in the size of pgmodeler logo name in .ui files

--- libs/libgui/src/tools/datahandlingform.cpp
+++ libs/libgui/src/tools/datahandlingform.cpp
@@ -28,6 +28,7 @@ DataHandlingForm::DataHandlingForm(QWidget * parent, Qt::WindowFlags f): QDialog
 	setupUi(this);
 	setWindowFlags(Qt::Dialog | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint);
 
+	CustomUiStyle::setStyleHint(CustomUiStyle::DefaultFrmHint, db_info_frm);
 	CustomUiStyle::setStyleHint(CustomUiStyle::DefaultFrmHint, separator_ln_2);
 
 	curr_grid_wgt = nullptr;
