aa990b6b945659f9b717b64c51065e6c55124429
Raphael Araújo e Silva <raphael@pgmodeler.io>
2025-12-19 17:23:28 -0300

    WIP: Refactor TabOrderManager to simplify widget discovery logic
    - Remove retrieveChildWidgets() recursive method
    - Consolidate widget filtering into single lambda in configureTabOrder()
    - Change getTabOrderList() signature from Hash to QWidgetList parameter
    - Use findChildren() without Qt::FindDirectChildrenOnly to get all widgets
    - Move widget class filtering to centralized removeIf() with ignored_classes list
    - Add filtering for qt_ internal widgets and QLineEditIconButton
    - Remove commented code and fix indentation inconsistency
    - Simplify architecture by eliminating complex container/basic widget classification

--- libs/libgui/src/utils/tabordermanager.cpp
+++ libs/libgui/src/utils/tabordermanager.cpp
@@ -80,66 +80,20 @@ bool TabOrderManager::eventFilter(QObject *object, QEvent *event)
 	return QObject::eventFilter(object, event);
 }
 
-void TabOrderManager::retrieveChildWidgets(QWidget *widget, QHash<QWidget *, QWidgetList> &ord_map)
+QWidgetList TabOrderManager::getTabOrderList(const QWidgetList &wgt_list)
 {
-	if(!widget)
-		return;
-
-	static const QStringList container_classes {
-		"QFrame", "QGroupBox", "QTabWidget"
-	},
-
-	basic_wgt_classes {
-		"QLineEdit", "QToolButton", "QPushButton",
-		"QCheckBox", "QRadioButton", "QSpinBox",
-		"QDoubleSpinBox", "QPlainTextEdit", "QComboBox"
-	};
-
-	QString wgt_class = widget->metaObject()->className();
-
-	if(container_classes.contains(wgt_class))
-	{
-		QWidgetList child_wgts;
-
-		if(wgt_class == QString("QTabWidget"))
-		{
-			QTabWidget *tab_wgt = qobject_cast<QTabWidget *>(widget);
-			child_wgts = tab_wgt->count() > 0 ?
-									 tab_wgt->currentWidget()->findChildren<QWidget *>() : QWidgetList();
-		}
-		else
-			child_wgts = widget->findChildren<QWidget *>(Qt::FindDirectChildrenOnly);
-
-		for(auto &wgt : child_wgts)
-			retrieveChildWidgets(wgt, ord_map);
-	}
-	else
-	{
-		QWidget *parent_wgt = nullptr;
-
-		if(!ord_map.contains(widget->parentWidget()) &&
-				widget->parentWidget() && ord_map.contains(widget->parentWidget()->parentWidget()))
-			parent_wgt = widget->parentWidget()->parentWidget();
-		else
-			parent_wgt = widget->parentWidget();
-
-		if(!basic_wgt_classes.contains(wgt_class))
-			ord_map[parent_wgt].append(widget->findChildren<QWidget *>(Qt::FindDirectChildrenOnly));
-		else
-			ord_map[parent_wgt].append(widget);
-	}
-}
-
-QWidgetList TabOrderManager::getTabOrderList(const QHash<QWidget *, QWidgetList> &ord_map)
-{
-	if(ord_map.isEmpty())
+	if(wgt_list.isEmpty())
 		return {};
 
+	QHash<QWidget *, QWidgetList> ord_map;
 	QMap<int, QMap<int, QWidgetList>> vh_ord_wgts;
 	QHash<QWidget *, QWidgetList> parent_ord_wgts;
 	QWidgetList tab_ord_list;
 	QPoint pos;
 	QWidget *root_wgt = qobject_cast<QWidget *>(parent());
+
+	for(auto &wgt : wgt_list)
+		ord_map[wgt->parentWidget()].append(wgt);
 
 	for(const auto &[parent_wgt, child_wgts] : ord_map.asKeyValueRange())
 	{
@@ -166,23 +120,14 @@ QWidgetList TabOrderManager::getTabOrderList(const QHash<QWidget *, QWidgetList>
 	for(const auto &[parent_wgt, child_wgts] : parent_ord_wgts.asKeyValueRange())
 	{
 		pos = root_wgt->mapFromGlobal(parent_wgt->mapToGlobal(parent_wgt->pos()));
-		//pos = parent_wgt->mapToGlobal(parent_wgt->pos());
 		vh_ord_wgts[pos.y()][pos.x()].append(parent_wgt);
 	}
 
 	for(const auto &[v_pos, h_ord_wgts] : vh_ord_wgts.asKeyValueRange())
 	{
 		for(const auto &[h_pos, wgts] : h_ord_wgts.asKeyValueRange())
-		{
-			//tab_ord_list.append(wgts);
 			tab_ord_list.append(parent_ord_wgts[wgts[0]]);
-		}
 	}
-
-	tab_ord_list.removeIf([](const QWidget *wgt){
-		return wgt->metaObject()->className() == QString("QLabel") ||
-					 !wgt->isVisible() || !wgt->isEnabled();
-	});
 
 	return tab_ord_list;
 }
@@ -193,25 +138,25 @@ void TabOrderManager::configureTabOrder()
 	cfg_timer.stop();
 
 	QWidget *parent_wgt = qobject_cast<QWidget *>(parent());
-	QWidgetList child_wgts = parent_wgt->findChildren<QWidget *>(Qt::FindDirectChildrenOnly);
-	QHash<QWidget *, QWidgetList> selected_wgts;
+	QWidgetList child_wgts = parent_wgt->findChildren<QWidget *>();
 
 	for(auto &wgt : tab_order_list)
 		wgt->removeEventFilter(this);
 
 	tab_order_list.clear();
 
-	for(auto &wgt : child_wgts)
-	{
-		wgt->setFocusPolicy(Qt::NoFocus);
+	child_wgts.removeIf([](const QWidget *wgt){
+		static const QStringList ignored_classes {
+			"QFrame", "QGroupBox", "QTabWidget", "QLabel",
+			"QLineEditIconButton", "QWidget"
+		};
 
-		if(!wgt->isVisible() || !wgt->isEnabled())
-			continue;
+		return ignored_classes.contains(wgt->metaObject()->className()) ||
+					 !wgt->isVisible() || !wgt->isEnabled() ||
+					 wgt->objectName().startsWith("qt_");
+	});
 
-		retrieveChildWidgets(wgt, selected_wgts);
-	}
-
-	tab_order_list = getTabOrderList(selected_wgts);
+	tab_order_list = getTabOrderList(child_wgts);
 
 	if(tab_order_list.isEmpty())
 		return;
@@ -221,7 +166,7 @@ void TabOrderManager::configureTabOrder()
 	for(int idx = 0; idx < count; idx++)
 	{
 		tab_order_list[idx]->installEventFilter(this);
-			tab_order_list[idx]->setFocusPolicy(Qt::StrongFocus);
+		tab_order_list[idx]->setFocusPolicy(Qt::StrongFocus);
 
 		if(idx <= (count - 2))
 			QWidget::setTabOrder(tab_order_list[idx], tab_order_list[idx + 1]);
--- libs/libgui/src/utils/tabordermanager.h
+++ libs/libgui/src/utils/tabordermanager.h
@@ -37,9 +37,7 @@ class TabOrderManager: public QObject {
 
 		QWidgetList tab_order_list;
 
-		void retrieveChildWidgets(QWidget *root_wgt, QHash<QWidget *, QWidgetList> &ord_map);
-
-		QWidgetList getTabOrderList(const QHash<QWidget *, QWidgetList> &ord_map);
+		QWidgetList getTabOrderList(const QWidgetList &wgt_list);
 
 	protected:
 		bool eventFilter(QObject *object, QEvent *event) override;
